235987fc13220a24c7ebd9651d4d3849bcaf8c81
fxwing <fxwing@126.com>
2021-04-12 11:28:35 +0800

    :ideas:  添加测试lodash文件

---
+++ base/curry.js
@@ -0,0 +1,6 @@
+"use strict";
+// 柯里化有用的场景
+exports.__esModule = true;
+var _ = require("lodash");
+var arr = [1, 2];
+console.log(_(arr).value());
---
+++ base/curry.ts
@@ -0,0 +1,5 @@
+// 柯里化有用的场景
+
+import * as _ from "lodash";
+const arr: number[] = [1, 2];
+console.log(_(arr).value());
